088060461bf31bdc91b3bacb0fa406cf00179906
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-14 07:28:15 -0800

    Add project files.

---
+++ threading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+class VariousMethods
+{
+    public static void Main()
+    {
+        Thread newThread = new Thread(new ThreadStart(TestMethod));
+        newThread.Start();
+        Thread.Sleep(1000);
+        Console.WriteLine("Main aborting new thread");
+        newThread.Abort("Information from Main");
+        newThread.Join();
+        Console.WriteLine("New thread terminated ");
+    }
+    static void TestMethod()
+    {
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("New thread");
+                Thread.Sleep(1000);
+            }
+        }
+        catch (ThreadAbortException abortException)
+        {
+            Console.WriteLine((string)abortException.ExceptionState);
+        }
+    }
+}
+
